c0fd4d845e284a327318bdb7a10fbbef1d5d47e1
Ilya <109240426+SarinXo@users.noreply.github.com>
2025-06-29 16:54:05 +0300

    ref: Минорные правки по PR'у

--- src/main/java/sarinxo/otpservice/entity/SendOtpEntity.java
+++ src/main/java/sarinxo/otpservice/entity/SendOtpEntity.java
@@ -11,9 +11,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.proxy.HibernateProxy;
 import sarinxo.otpservice.entity.base.AuditableEntity;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -84,6 +86,28 @@ public class SendOtpEntity extends AuditableEntity {
      */
     public enum MessageStatus {
         IN_PROCESS, DELIVERED, ERROR
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        SendOtpEntity that = (SendOtpEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
     }
 
 }
